55d8ef2cce894cd0c9032eb4d849218d513f196a
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2017-06-10 00:50:33 +0900

    Beautified

--- Dockerfile
+++ Dockerfile
@@ -2,37 +2,37 @@ FROM python:2.7
 
 # Install NGINX
 RUN set -ex \
-		&& apt-get update -y && apt-get install -y --no-install-recommends \
-			nginx \
-		&& rm -rf /var/lib/apt/lists/* \
-		&& rm -rf /var/www/html/* \
-		&& rm -rf /etc/nginx/sites-available/* \
-		&& rm -rf /etc/nginx/sites-enabled/*
+    && apt-get update -y && apt-get install -y --no-install-recommends \
+        nginx \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -rf /var/www/html/* \
+    && rm -rf /etc/nginx/sites-available/* \
+    && rm -rf /etc/nginx/sites-enabled/*
 
 COPY nginx/default.conf /etc/nginx/conf.d/
 
 # Install uWSGI
 RUN set -ex \
-		&& pip install --no-cache-dir uwsgi
+    && pip install --no-cache-dir uwsgi
 
 # Install MoinMoin
 ENV MOINMOIN_VERSION 1.9.9
 
 RUN set -ex \
-		&& pip install --no-cache-dir "moin==$MOINMOIN_VERSION" \
-		&& mkdir -p \
-			/usr/local/etc/moin \
-			/usr/local/run/moin \
-			/var/www/moin \
+    && pip install --no-cache-dir "moin==$MOINMOIN_VERSION" \
+    && mkdir -p \
+        /usr/local/etc/moin \
+        /usr/local/run/moin \
+        /var/www/moin \
     && chown -R www-data:www-data \
-			/usr/local/run/moin
+        /usr/local/run/moin
 
 COPY moin/uwsgi.ini /usr/local/etc/moin/
 
 # Install Supervisord
 RUN set -ex \
-		&& pip install --no-cache-dir supervisor \
-		&& mkdir -p /usr/local/run/supervisor
+    && pip install --no-cache-dir supervisor \
+    && mkdir -p /usr/local/run/supervisor
 
 COPY supervisord/supervisord.conf /usr/local/etc/
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,26 +3,26 @@ set -eu
 
 # generate default site if not present
 if [ ! -d /var/www/moin/config ] && \
-   [ ! -d /var/www/moin/data ] && \
-   [ ! -d /var/www/moin/server ] && \
-   [ ! -d /var/www/moin/underlay ];
+[ ! -d /var/www/moin/data ] && \
+[ ! -d /var/www/moin/server ] && \
+[ ! -d /var/www/moin/underlay ];
 then
-  cp -r /usr/local/share/moin/* /var/www/moin
-  sed -i "/^#sys.path.insert(0, '\/path\/to\/farmconfigdir')/a sys.path.insert(0, '\/var\/www\/moin\/config')" /var/www/moin/server/*
-  sed -i "s/^#from MoinMoin import log/from MoinMoin import log/" /var/www/moin/server/*
-  sed -i "s/^#log.load_config('\/path\/to\/logging_configuration_file')/log.load_config('\/var\/www\/moin\/config\/logging\/logfile')/" /var/www/moin/server/*
-  sed -i "s/instance_dir = wikiconfig_dir/instance_dir = os.path.join(wikiconfig_dir, os.pardir)/" /var/www/moin/config/wikiconfig.py
-  sed -i 's/#superuser = \[u"YourName", \]/superuser = \[u"root", \]/' /var/www/moin/config/wikiconfig.py
+    cp -r /usr/local/share/moin/* /var/www/moin
+    sed -i "/^#sys.path.insert(0, '\/path\/to\/farmconfigdir')/a sys.path.insert(0, '\/var\/www\/moin\/config')" /var/www/moin/server/*
+    sed -i "s/^#from MoinMoin import log/from MoinMoin import log/" /var/www/moin/server/*
+    sed -i "s/^#log.load_config('\/path\/to\/logging_configuration_file')/log.load_config('\/var\/www\/moin\/config\/logging\/logfile')/" /var/www/moin/server/*
+    sed -i "s/instance_dir = wikiconfig_dir/instance_dir = os.path.join(wikiconfig_dir, os.pardir)/" /var/www/moin/config/wikiconfig.py
+    sed -i 's/#superuser = \[u"YourName", \]/superuser = \[u"root", \]/' /var/www/moin/config/wikiconfig.py
 fi
 
 # correct permissions
 chown -R www-data:www-data \
-  /var/www/moin \
-  /var/log/moin \
-  /var/log/nginx
+/var/www/moin \
+/var/log/moin \
+/var/log/nginx
 chmod -R ug+rwX \
-  /var/www/moin \
-  /var/log/moin \
-  /var/log/nginx
+/var/www/moin \
+/var/log/moin \
+/var/log/nginx
 
 exec "$@"
